c061e6c5a694f34033f783967324e187125dc8bd
Julien <julien@canevas.eu>
2020-12-03 16:34:52 +0100

    day 3 minor fixes

--- src/bin/day_three.rs
+++ src/bin/day_three.rs
@@ -8,36 +8,39 @@ struct ForestMap {
 }
 
 fn read_input() -> Result<ForestMap,Error>{
-    let f = File::open("input_day_three_test")?;
-    let mut topo = Vec::new();
-    let file = BufReader::new(f);
-    for line in file.lines(){
-        topo.push(line.unwrap());
-    }
-    let map = ForestMap {
-        width : topo[0].len(),
-        height : topo.len(),
-        topo : topo
+    let f = match File::open("input_day_three") {
+        Ok(f) => f,
+        Err(_) => { println!{"No file found"};
+            std::process::exit(-1);}
     };
-    Ok(map)
+    let mut topo = Vec::new();
+        for line in BufReader::new(f).lines(){
+            topo.push(line.unwrap());
+        }
+        let map = ForestMap {
+            width : topo[0].len(),
+            height : topo.len(),
+            topo : topo
+        };
+        Ok(map)
 }
 
 fn weeeee(map:&ForestMap, right: usize,down: usize)
     -> usize{
-    let mut trees = 0;
-    let mut x = 0;
-    for y in (down..map.height).step_by(down){
-        x = (x + right)%map.width;
-        let pouet = map.topo[y]
-            .chars()
-            .nth(x%map.width)
-            .unwrap();
-        if pouet == '#'{
+        let mut trees = 0;
+        let mut x = 0;
+        for y in (down..map.height).step_by(down){
+            x = (x + right)%map.width;
+            let pouet = map.topo[y]
+                .chars()
+                .nth(x%map.width)
+                .unwrap();
+            if pouet == '#'{
                 trees += 1;
             }
+        }
+        trees
     }
-    trees
-}
 
 
 fn main() {
@@ -49,6 +52,5 @@ fn main() {
     let treess = slopes
         .iter()
         .fold(1, |acc, &t| acc*weeeee(&map, t.0, t.1));
-    println!("Total number of trees met during descends:
-    {}",treess);
+    println!("Total number of trees met during descends: {}",treess);
 }
