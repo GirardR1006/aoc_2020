d593bedbeb57a9e2dd4a88a7f63fa2970d88ec65
Julien Girard <julien.girard2@cea.fr>
2020-12-14 17:45:32 +0100

    day 7 suspended for now, day 8 done

--- src/bin/day_eight.rs
+++ src/bin/day_eight.rs
@@ -11,6 +11,21 @@ enum Instruction{
     Jmp(i32),
     Nop(i32)
 }
+// change the instruction at index id and reset the program
+// execution state
+fn change_instr(instr:&Instruction)->Instruction{
+    match instr{
+        Instruction::Acc(i) =>{
+            Instruction::Acc(*i)
+        },
+        Instruction::Jmp(i) =>{
+            Instruction::Nop(*i)
+        },
+        Instruction::Nop(i) =>{
+            Instruction::Jmp(*i)
+        }
+    }
+}
 #[derive(Debug)]
 struct Program{
     instructions: Vec<(Instruction,bool)>,
@@ -18,32 +33,67 @@ struct Program{
     count: i32
 }
 impl Program{
-    fn execute(self: &mut Program) -> i32{
+    fn find_terminal_state(self: &mut Program) -> i32{
+        let mut id_to_mod = 0;
+        let mut acc = 0;
+        let mut terminated = false;
+        println!("Initial program {:?}",self);
+        while !terminated{
+            let res = self.execute();
+            acc = res.0;
+            terminated = res.2;
+            self.mutate(id_to_mod);
+            id_to_mod += 1;
+        }
+        println!("Found instruction to modify at index: {}", id_to_mod);
+        acc
+    }
+    fn mutate(self: &mut Program, id: usize) {
+        let (cur_instr,_) = &self.instructions[id];
+        self.instructions[id] = (change_instr(cur_instr),false);
+        //revert change made previously
+        if id > 0{
+            let (prev_instr,_) = &self.instructions[id-1];
+            self.instructions[id-1] = (change_instr(prev_instr),false)
+        }
+        // resetting all visited instructions
+        for (_,switch) in &mut self.instructions {
+            *switch = false;
+        };
+        self.count = 0;
+        self.current_instruction = 0;
+
+    }
+    fn execute(self: &mut Program) -> (i32,bool,bool){
         let mut infinite = false;
-        while !infinite {
-            let (cur_instr,switch) = &mut self.instructions[
-                self.current_instruction as usize];
-            if *switch {infinite = true}
+        let mut terminated = false;
+        while !infinite && !terminated {
+            let num_instructions = self.instructions.len().clone();
+            let cur_idx = self.current_instruction as usize;
+            if cur_idx >= num_instructions {terminated = true}
             else{
-                match cur_instr {
-                    Instruction::Acc(i) =>{
-                        *switch = true;
-                        self.count += *i;
-                        self.current_instruction += 1;
-                    },
-                    Instruction::Jmp(i) =>{
-                        *switch = true;
-                        self.current_instruction += *i;
-                    },
-                    Instruction::Nop(_i) =>{
-                        *switch = true;
-                        self.current_instruction += 1;
+                let (cur_instr,switch) = &mut self.instructions[cur_idx];
+                if *switch {infinite = true}
+                else{
+                    match cur_instr {
+                        Instruction::Acc(i) =>{
+                            *switch = true;
+                            self.count += *i;
+                            self.current_instruction += 1
+                        },
+                        Instruction::Jmp(i) =>{
+                            *switch = true;
+                            self.current_instruction += *i
+                        },
+                        Instruction::Nop(_i) =>{
+                            *switch = true;
+                            self.current_instruction += 1
+                        }
                     }
                 }
             };
         }
-        println!("Infinite loop spotted.");
-        self.count
+        (self.count,infinite,terminated)
     }
 }
 
@@ -95,6 +145,6 @@ Result<Program,Error>{
 
 fn main(){
     let mut program = parse().unwrap();
-    println!("value of the accumulator: {}",
-             program.execute())
+    println!("value of the accumulator after mutation: {}",
+             program.find_terminal_state())
 }
